622e9bc3f3c4318fba0f8058b09b35ade448a56e
Roma1558 <romualdo706@gmail.com>
2022-06-16 16:17:35 -0300

    Funcionário - Implementação

---
+++ src/gui/FuncionarioListController.java
@@ -0,0 +1,171 @@
+package gui;
+
+import java.net.URL;
+import java.util.List;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+import application.Main;
+import db.DbIntegrityException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Funcionario;
+import model.services.FuncionarioService;
+
+public class FuncionarioListController implements Initializable, DataChangeListener {
+
+	private FuncionarioService service;
+
+	@FXML
+	private TableView<Funcionario> tableViewFuncionario;
+
+	@FXML
+	private TableColumn<Funcionario, Integer> tableColumnId;
+
+	@FXML
+	private TableColumn<Funcionario, String> tableColumnNome;
+
+	@FXML
+	private TableColumn<Funcionario, Funcionario> tableColumnEDIT;
+
+	@FXML
+	private TableColumn<Funcionario, Funcionario> tableColumnREMOVE;
+
+	@FXML
+	private Button btNew;
+
+	private ObservableList<Funcionario> obsList;
+
+	@FXML
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		Funcionario obj = new Funcionario();
+		createDialogForm(obj, "/gui/FuncionarioForm.fxml", parentStage);
+	}
+
+	public void setFuncionarioService(FuncionarioService service) {
+		this.service = service;
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() {
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tableColumnNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+
+		Stage stage = (Stage) Main.getMainScene().getWindow();
+		tableViewFuncionario.prefHeightProperty().bind(stage.heightProperty());
+
+	}
+
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("Serviço nulo");
+		}
+		List<Funcionario> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewFuncionario.setItems(obsList);
+		initEditButtons();
+		initRemoveButtons();
+	}
+
+	private void createDialogForm(Funcionario obj, String absoluteName, Stage parentStage) {
+		/*try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+
+			FuncionarioFormController controller = loader.getController();
+			controller.setFuncionario(obj);
+			controller.setFuncionarioService(new FuncionarioService());
+			controller.subcribeDataChangeListener(this);
+			controller.updateFormData();
+
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Novo Departamento");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Erro ao carregar a view", e.getMessage(), AlertType.ERROR);
+		}*/
+	}
+
+	@Override
+	public void onDataChanged() {
+		updateTableView();
+	}
+
+	private void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Funcionario, Funcionario>() {
+			private final Button button = new Button("Editar");
+
+			@Override
+			protected void updateItem(Funcionario obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(
+						event -> createDialogForm(obj, "/gui/FuncionarioForm.fxml", Utils.currentStage(event)));
+			}
+		});
+	}
+
+	private void initRemoveButtons() {
+		tableColumnREMOVE.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnREMOVE.setCellFactory(param -> new TableCell<Funcionario, Funcionario>() {
+			private final Button button = new Button("Deletar");
+
+			@Override
+			protected void updateItem(Funcionario obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(event -> removeEntity(obj));
+			}
+		});
+	}
+
+	private void removeEntity(Funcionario obj) {
+		Optional<ButtonType> result = Alerts.showConfirmation("Confirmação", "Você tem certeza que deseja deletar?");
+		
+		if (result.get() == ButtonType.OK) {
+			if (service == null) {
+				throw new IllegalStateException("Serviço nulo");
+			}
+			try {
+				service.remove(obj);
+				updateTableView();
+			}
+			catch (DbIntegrityException e) {
+				Alerts.showAlert("Erro ao remover objeto", null, e.getMessage(), Alert.AlertType.ERROR);
+			}
+		}
+	}
+}
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
+import model.services.FuncionarioService;
 
 public class MainViewController implements Initializable {
 
@@ -31,7 +32,10 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemFuncionarioAction() {
-		System.out.println("onMenuItemFuncionarioAction");
+		loadView("/gui/FuncionarioList.fxml", (FuncionarioListController controller) -> {
+			controller.setFuncionarioService(new FuncionarioService());
+			controller.updateTableView();
+		});
 	}
 	
 	@FXML
